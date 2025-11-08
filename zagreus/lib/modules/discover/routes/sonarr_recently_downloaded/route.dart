53e1f9e245b5cd601a7cf7895643009ecd6df761
Umikaze <cjsclark@icloud.com>
2025-11-08 00:46:11 -0700

    Adjust padding in SonarrRecentlyDownloadedRoute
    Changed the padding in the Expanded widget from uniform (all sides) to horizontal only, removing top and bottom padding for improved layout consistency.

--- zagreus/lib/modules/discover/routes/sonarr_recently_downloaded/route.dart
+++ zagreus/lib/modules/discover/routes/sonarr_recently_downloaded/route.dart
@@ -278,7 +278,7 @@ class _State extends State<SonarrRecentlyDownloadedRoute>
                 // Content
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.all(12),
+                    padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
