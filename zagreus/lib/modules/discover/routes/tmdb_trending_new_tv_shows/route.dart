3991e3e440402c780f5cec7b030cef38298e600c
Umikaze <cjsclark@icloud.com>
2025-11-11 03:18:56 -0700

    Adjust grid child aspect ratio for TV shows
    Changed the childAspectRatio in the SliverGridDelegate from 0.634 to 0.58 to improve the layout of trending new TV shows in the grid view.

--- zagreus/lib/modules/discover/routes/tmdb_trending_new_tv_shows/route.dart
+++ zagreus/lib/modules/discover/routes/tmdb_trending_new_tv_shows/route.dart
@@ -287,7 +287,7 @@ class _State extends State<TMDBTrendingNewTVShowsRoute>
         ),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: usesThreeColumns ? 3 : 2,
-          childAspectRatio: 0.634,
+          childAspectRatio: 0.58,
           crossAxisSpacing: gridSpacing,
           mainAxisSpacing: gridSpacing,
         ),
