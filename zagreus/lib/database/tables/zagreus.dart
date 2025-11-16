9e0de870e56fc79f5ff4406db3f8d4dd2c5eba20
Umikaze <cjsclark@icloud.com>
2025-11-16 10:25:06 -0700

    Set MODULE_SWITCHER_FAB_ENABLED default to false
    Changed the default value of MODULE_SWITCHER_FAB_ENABLED from true to false in the Zagreus database table enum. This likely disables the module switcher FAB by default for new users or installations.

--- zagreus/lib/database/tables/zagreus.dart
+++ zagreus/lib/database/tables/zagreus.dart
@@ -16,7 +16,7 @@ enum ZagreusDatabase<T> with ZagTableMixin<T> {
   DRAWER_MANUAL_ORDER<List>([]),
   ENABLED_PROFILE<String>(ZagProfile.DEFAULT_PROFILE),
   DOWNLOADS_DRAWER_ENABLED<bool>(true),
-  MODULE_SWITCHER_FAB_ENABLED<bool>(true),
+  MODULE_SWITCHER_FAB_ENABLED<bool>(false),
   MODULE_TAB_MEMORY_ENABLED<bool>(true),
   NETWORKING_TLS_VALIDATION<bool>(false),
   NETWORKING_LOCAL_SWITCHING_ENABLED<bool>(false),
